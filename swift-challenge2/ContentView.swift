531e75a09a5e76a8d21399ef13a1879dec3bca75
claire / seal <claire_lai_2028@mgs.sch.edu.sg>
2025-08-02 16:23:19 +0800

    Added the small map preview thing, going to add permissions now

--- swift-challenge2/ContentView.swift
+++ swift-challenge2/ContentView.swift
@@ -6,16 +6,22 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+    @StateObject private var locationManager = LocationManager()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            if let location = locationManager.location {
+                Map(coordinateRegion: .constant(MKCoordinateRegion(center: location.coordinate, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))), showsUserLocation: true)
+                    .clipShape(.circle) // Making the small map a circle
+                    .frame(width: 250, height: 250)
+                    .shadow(radius: 10)
+            } else {
+                ProgressView("Hang tight - we're currently locating you!")
+            }
         }
-        .padding()
     }
 }
 
