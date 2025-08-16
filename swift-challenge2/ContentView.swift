7d4a521eb5d1e3d77ab96fbb27d1227a51e28d76
claire / seal <claire_lai_2028@mgs.sch.edu.sg>
2025-08-16 13:41:02 +0800

    Changed shadow radius, I preset a location for the simulator and fixed the map preview issue

--- swift-challenge2/ContentView.swift
+++ swift-challenge2/ContentView.swift
@@ -17,7 +17,7 @@ struct ContentView: View {
                 Map(coordinateRegion: .constant(MKCoordinateRegion(center: location.coordinate, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))), showsUserLocation: true)
                     .clipShape(.circle) // Making the small map a circle
                     .frame(width: 250, height: 250)
-                    .shadow(radius: 10)
+                    .shadow(radius: 15)
             } else {
                 ProgressView("Hang tight - we're currently locating you!")
                     .padding()
