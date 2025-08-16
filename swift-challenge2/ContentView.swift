365375f0b0c993126b0c541c173873f32848798f
claire / seal <claire_lai_2028@mgs.sch.edu.sg>
2025-08-16 13:53:16 +0800

    Added the navigation link to the primary page (also removed the back button from it), started coding the primary page

--- swift-challenge2/ContentView.swift
+++ swift-challenge2/ContentView.swift
@@ -12,16 +12,23 @@ struct ContentView: View {
     @StateObject private var locationManager = LocationManager()
     
     var body: some View {
-        ZStack {
-            if let location = locationManager.location {
-                Map(coordinateRegion: .constant(MKCoordinateRegion(center: location.coordinate, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))), showsUserLocation: true)
-                    .clipShape(.circle) // Making the small map a circle
-                    .frame(width: 250, height: 250)
-                    .shadow(radius: 15)
-            } else {
-                ProgressView("Hang tight - we're currently locating you!")
-                    .padding()
+        NavigationStack {
+            ZStack {
+                if let location = locationManager.location {
+                    Map(coordinateRegion: .constant(MKCoordinateRegion(center: location.coordinate, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))), showsUserLocation: true)
+                        .clipShape(.circle) // Making the small map a circle
+                        .frame(width: 250, height: 250)
+                        .shadow(radius: 15)
+                } else {
+                    ProgressView("Hang tight - we're currently locating you! When you see the map, it means we've finished :)")
+                        .padding()
+                }
             }
+            NavigationLink ("Done! Next >") {
+                RouteInputView()
+                    .navigationBarBackButtonHidden(true)
+            }
+            .padding()
         }
     }
 }
---
+++ swift-challenge2/RouteInputView.swift
@@ -0,0 +1,22 @@
+//
+//  RouteInputView.swift
+//  swift-challenge2
+//
+//  Created by T Krobot on 16/8/25.
+//
+
+import SwiftUI
+
+struct RouteInputView: View {
+    var body: some View {
+        NavigationStack {
+            Text("Let's plan your route!")
+                .font(.largeTitle)
+            
+        }
+    }
+}
+
+#Preview {
+    RouteInputView()
+}
